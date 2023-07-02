f371e0d247e7b727935c80e1f5c00fae40b8190b
grace <grace994084@gmail.com>
2023-07-02 17:15:29 +0900

    feat : recipe item component 생성

---
+++ src/components/RecipeItem.js
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
+function RecipeItem({ recipeItem }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="recipe-item">
+      <div className={`taste-img-wrapper taste-${recipeItem.taste}`}>
+        <img
+          src={process.env.PUBLIC_URL + `assets/taste${recipeItem.taste}.png`}
+          alt={`taste-img-${recipeItem.taste}`}
+        />
+      </div>
+      <div className="info-wrapper">
+        <div className="recipe-date">
+          {new Date(parseInt(recipeItem.date)).toLocaleDateString()}
+        </div>
+        <div className="recipe-content-title">{recipeItem.title}</div>
+      </div>
+      <div className="btn-wrapper">
+        <MyButton
+          text={"Edit"}
+          onClick={() => navigate(`/edit/${recipeItem.id}`)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default RecipeItem;
