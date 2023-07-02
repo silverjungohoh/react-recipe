92ce4d3543714f38eaffa3c75e7b58fe8ac80469
grace <grace994084@gmail.com>
2023-07-02 21:05:53 +0900

    feat : taste item component 생성

---
+++ src/components/TasteItem.js
@@ -0,0 +1,13 @@
+function TasteItem({ tasteItem, onClick, isSelected }) {
+  return (
+    <div
+      className={`taste-item ${isSelected ? "select" : ""}`}
+      onClick={() => onClick(tasteItem.tasteId)}
+    >
+      <img src={tasteItem.tasteImg} alt={`taste-img-${tasteItem.tasteId}`} />
+      <span>{tasteItem.tasteDesc}</span>
+    </div>
+  );
+}
+
+export default TasteItem;
