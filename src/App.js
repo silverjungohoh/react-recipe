5785cc8cca1a7203c1e246920038b511be5c48ef
grace <grace994084@gmail.com>
2023-07-03 03:49:57 +0900

    feat : recipe  state 관리 및 page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,100 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import New from "./pages/New";
+import Recipe from "./pages/Recipe";
+import React, { useReducer, useRef } from "react";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const newItem = {
+        ...action.data,
+      };
+      newState = [newItem, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const RecipeStateContext = React.createContext();
+export const RecipeDispatchContext = React.createContext();
+
 function App() {
-  return <div className="App"></div>;
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(1);
+
+  // create
+  const onCreate = (date, title, content, taste) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        title,
+        content,
+        taste,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  // remove
+  const onRemove = (targetId) => {
+    dispatch({
+      type: "REMOVE",
+      targetId,
+    });
+  };
+
+  // edit
+  const onEdit = (targetId, date, title, content, taste) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        title,
+        content,
+        taste,
+      },
+    });
+  };
+
+  return (
+    <RecipeStateContext.Provider value={data}>
+      <RecipeDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/recipe/:id" element={<Recipe />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </RecipeDispatchContext.Provider>
+    </RecipeStateContext.Provider>
+  );
 }
 
 export default App;
